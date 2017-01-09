87b7d1d702395ed019a5d361b6d13091f6af0bf6
alura <alura@Alura-Azul.local>
2017-01-09 15:53:04 -0200

    aula1-video2-importando-html

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/reset.css';
+import './css/timeline.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
