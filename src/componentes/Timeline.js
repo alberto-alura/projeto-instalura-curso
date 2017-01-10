eb4fc4591f3240ce5dd30ff85a6a302ec1d090ec
alura <alura@Alura-Azul.local>
2017-01-10 15:12:15 -0200

    aula2-video1-um-pouco-de-promise-fetch

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Foto from './Foto';
 
 export default class Timeline extends Component {
+
+    componentDidMount(){
+      fetch('http://localhost:8080/api/public/fotos/alots');
+    }
+
     render(){
         return (
         <div className="fotos container">
