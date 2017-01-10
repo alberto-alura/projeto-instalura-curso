e96cb9e75cdc85c55af1abe16d3e1deb78157c4c
alura <alura@Alura-Azul.local>
2017-01-10 14:31:10 -0200

    aula1-video4-dividindo-ainda-mais

---
+++ src/componentes/Foto.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+class FotoAtualizacoes extends Component {
+    render(){
+        return (
+            <section className="fotoAtualizacoes">
+              <a href="#" className="fotoAtualizacoes-like">Likar</a>
+              <form className="fotoAtualizacoes-form">
+                <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
+                <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
+              </form>
+
+            </section>            
+        );
+    }
+}
+
+class FotoInfo extends Component {
+    render(){
+        return (
+            <div className="foto-in fo">
+              <div className="foto-info-likes">
+
+                <a href="#">
+                  alots_ssa
+                </a>
+
+                ,
+
+                <a href="#">
+                  rafael_rollo
+                </a> 
+
+                 curtiram
+             
+              </div>
+
+              <p className="foto-info-legenda">
+                <a className="foto-info-autor">autor </a>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+              </p>
+
+              <ul className="foto-info-comentarios">
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
+                </li>
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
+                </li>
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
+                </li>
+              </ul>
+            </div>            
+        );
+    }
+}
+
+class FotoHeader extends Component {
+    render(){
+        return (
+            <header className="foto-header">
+              <figure className="foto-usuario">
+                <img src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-19/11199408_569104449895751_1837574990_a.jpg" alt="foto do usuario"/>
+                <figcaption className="foto-usuario">
+                  <a href="#">
+                    alots
+                  </a>  
+                </figcaption>
+              </figure>
+              <time className="foto-data">03/10/2016 20:13</time>
+            </header>            
+        );
+    }
+}
+
+export default class Foto extends Component {
+    render(){
+        return (
+          <div className="foto">
+            <FotoHeader/>
+            <img alt="foto" className="foto-src" src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-15/e35/14482111_1635089460122802_8984023070045896704_n.jpg?ig_cache_key=MTM1MzEzNjM4NzAxMjIwODUyMw%3D%3D.2"/>
+            <FotoInfo/>
+            <FotoAtualizacoes/>
+          </div>            
+        );
+    }
+}
