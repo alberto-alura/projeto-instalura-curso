c19589931c42914a1339d7e4e306130db33dd081
alura <alura@Alura-Azul.local>
2017-01-11 16:12:39 -0200

    aula3-video2-logando-api

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+
+    constructor(){
+        super();
+        this.state = {msg:''};
+    }
+
+    envia(event){
+        event.preventDefault();
+
+        const requestInfo = {
+            method:'POST',
+            body:JSON.stringify({login:this.login.value,senha:this.senha.value}),
+            headers:new Headers({
+                'Content-type' : 'application/json' 
+            })
+        };
+
+        fetch('http://localhost:8080/api/public/login',requestInfo)
+            .then(response => {
+                if(response.ok) {
+                    return response.text();
+                } else {
+                    this.setState({msg:'não foi possível fazer o login'});
+                }
+            })
+            .then(token => {
+                console.log(token);
+            });
+    }
+
     render(){
         return (
             <div className="login-box">
                 <h1 className="header-logo">Instalura</h1>
-                <form>
-                    <input type="text"/>
-                    <input type="password"/>
+                <span>{this.state.msg}</span>
+                <form onSubmit={this.envia.bind(this)}>
+                    <input type="text" ref={(input) => this.login = input}/>
+                    <input type="password" ref={(input) => this.senha = input}/>
                     <input type="submit" value="login"/>
                 </form>
             </div>
