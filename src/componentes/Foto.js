23131ea5d92ab6eb872f794751a1bfe5941befa0
alura <alura@Alura-Azul.local>
2017-01-12 18:55:43 -0200

    aula5-video1-likeando-parte1

--- src/componentes/Foto.js
+++ src/componentes/Foto.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import {Link} from 'react-router';
 
 class FotoAtualizacoes extends Component {
+
+    constructor(props){
+      super(props);       
+      this.state = {likeada : this.props.foto.likeada};
+    }
+
+    like(event){
+      event.preventDefault();
+
+      fetch(`http://localhost:8080/api/fotos/${this.props.foto.id}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`,{method:'POST'})
+        .then(response => {
+          if(response.ok) {
+            return response.json();
+          } else {            
+            throw new Error("não foi possível realizar o like da foto");
+          }
+        })
+        .then(liker => {
+          this.setState({likeada : !this.state.likeada});
+        });
+    }
+
     render(){
         return (
             <section className="fotoAtualizacoes">
-              <a href="#" className="fotoAtualizacoes-like">Likar</a>
+              <a onClick={this.like.bind(this)} className={this.state.likeada ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like'}>Likar</a>
               <form className="fotoAtualizacoes-form">
                 <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
                 <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
@@ -78,7 +100,7 @@ export default class FotoItem extends Component {
             <FotoHeader foto={this.props.foto}/>
             <img alt="foto" className="foto-src" src={this.props.foto.urlFoto}/>
             <FotoInfo foto={this.props.foto}/>
-            <FotoAtualizacoes/>
+            <FotoAtualizacoes foto={this.props.foto}/>
           </div>            
         );
     }
