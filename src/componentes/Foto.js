b4cd9f624ee85a3d88f3994ea75b7a071b5204d5
alura <alura@Alura-Azul.local>
2017-01-13 16:26:49 -0200

    aula5-video3-implementacao-comentarios

--- src/componentes/Foto.js
+++ src/componentes/Foto.js
@@ -26,12 +26,37 @@ class FotoAtualizacoes extends Component {
         });
     }
 
+    comenta(event){
+      event.preventDefault();
+
+
+      const requestInfo = {
+        method:'POST',
+        body:JSON.stringify({texto:this.comentario.value}),
+        headers: new Headers({
+          'Content-type':'application/json'
+        })
+      };
+
+      fetch(`http://localhost:8080/api/fotos/${this.props.foto.id}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`,requestInfo)
+        .then(response => {
+          if(response.ok){
+            return response.json();
+          } else {
+            throw new Error("não foi possível comentar");
+          }
+        })
+        .then(novoComentario => {
+          Pubsub.publish('novos-comentarios',{fotoId:this.props.foto.id,novoComentario});
+        })
+    }
+
     render(){
         return (
             <section className="fotoAtualizacoes">
               <a onClick={this.like.bind(this)} className={this.state.likeada ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like'}>Likar</a>
-              <form className="fotoAtualizacoes-form">
-                <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
+              <form className="fotoAtualizacoes-form" onSubmit={this.comenta.bind(this)}>
+                <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" ref={input => this.comentario = input}/>
                 <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
               </form>
 
@@ -44,7 +69,7 @@ class FotoInfo extends Component {
 
     constructor(props){
       super(props);
-      this.state = {likers : this.props.foto.likers};
+      this.state = {likers : this.props.foto.likers,comentarios:this.props.foto.comentarios};
     }
 
     componentWillMount(){
@@ -58,6 +83,13 @@ class FotoInfo extends Component {
             const novosLikers = this.state.likers.filter(liker => liker.login !== infoLiker.liker.login);
             this.setState({likers:novosLikers});
           }
+        }
+      });
+
+      Pubsub.subscribe('novos-comentarios',(topico,infoComentario) => {
+        if(this.props.foto.id === infoComentario.fotoId){
+          const novosComentarios = this.state.comentarios.concat(infoComentario.novoComentario);
+          this.setState({comentarios:novosComentarios});
         }
       });
     }
@@ -83,7 +115,7 @@ class FotoInfo extends Component {
 
               <ul className="foto-info-comentarios">
                 {
-                  this.props.foto.comentarios.map(comentario => {
+                  this.state.comentarios.map(comentario => {
                     return (
                       <li className="comentario" key={comentario.id}>
                         <Link to={`/timeline/${comentario.login}`} className="foto-info-autor">{comentario.login} </Link>
