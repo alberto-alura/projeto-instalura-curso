b7d47a99a7d1fed6ab8c3f1e2df516cffe1942da
alura <alura@Alura-Azul.local>
2017-01-13 17:21:45 -0200

    aula5-video4-implementacao-pesquisa

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import Pubsub from 'pubsub-js';
 
 export default class Header extends Component {
+
+    pesquisa(event){
+      event.preventDefault();
+      fetch(`http://localhost:8080/api/public/fotos/${this.loginPesquisado.value}`)
+        .then(response => response.json())
+        .then(fotos => {
+          Pubsub.publish('timeline',fotos);
+        });
+    }
 
     render(){
         return (
@@ -9,8 +19,8 @@ export default class Header extends Component {
             Instalura
           </h1>
 
-          <form className="header-busca">
-            <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo"/>
+          <form className="header-busca" onSubmit={this.pesquisa.bind(this)}>
+            <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo" ref={input => this.loginPesquisado = input}/>
             <input type="submit" value="Buscar" className="header-busca-submit"/>
           </form>
 
--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FotoItem from './Foto';
+import Pubsub from 'pubsub-js';
 
 export default class Timeline extends Component {
 
@@ -7,6 +8,12 @@ export default class Timeline extends Component {
       super(props);
       this.state = {fotos:[]};
       this.login = this.props.login;
+    }
+
+    componentWillMount(){
+      Pubsub.subscribe('timeline',(topico,fotos) => {
+        this.setState({fotos});
+      })
     }
 
     carregaFotos(){  
