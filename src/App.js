0f3089b04420e033c82a5d41f42cb7554514d3a0
alura <alura@Alura-Azul.local>
2017-02-02 16:02:29 -0200

    react3-aula2.1-inicio-redux

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Timeline from './componentes/Timeline';
 import TimelineStore from './logicas/TimelineStore'
+import {createStore} from 'redux';
 
 const timelineStore = new TimelineStore([]);
+
+//REDUCER
+function timeline(state=[],action){
+  if(action.type === 'LISTAGEM'){  
+    return action.fotos;
+  }
+
+  return state;
+}
+
+const store = createStore(timeline);
 
 class App extends Component {
   render() {    
@@ -11,7 +23,7 @@ class App extends Component {
     <div id="root">
       <div className="main">
         <Header/>
-        <Timeline login={this.props.params.login} store={timelineStore}/>
+        <Timeline login={this.props.params.login} store={store}/>
       </div>
     </div>
     );
--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -11,8 +11,8 @@ export default class Timeline extends Component {
     }
 
     componentWillMount(){
-      this.props.store.subscribe(fotos => {
-        this.setState({fotos});
+      this.props.store.subscribe(() => {
+        this.setState({fotos:this.props.store.getState()});
       })
     }
 
@@ -23,8 +23,10 @@ export default class Timeline extends Component {
         urlPerfil = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
       } else {
         urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
-      }      
-      this.props.store.lista(urlPerfil);
+      } 
+      const listaFixa = [{"urlPerfil":"https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-19/11199408_569104449895751_1837574990_a.jpg","loginUsuario":"alots","horario":"05/12/2016 16:21","urlFoto":"https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-15/e35/14482111_1635089460122802_8984023070045896704_n.jpg?ig_cache_key=MTM1MzEzNjM4NzAxMjIwODUyMw%3D%3D.2","id":1,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto"},{"urlPerfil":"https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-19/11199408_569104449895751_1837574990_a.jpg","loginUsuario":"alots","horario":"05/12/2016 16:21","urlFoto":"https://instagram.fcgh9-1.fna.fbcdn.net/t51.2885-15/e35/15276770_381074615568085_8052939980646907904_n.jpg?ig_cache_key=MTM5ODY4MDMyNjYyMDA1MDE4OQ%3D%3D.2","id":2,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto"}];     
+
+      this.props.store.dispatch({type:'LISTAGEM',fotos:listaFixa});      
     }
 
     componentDidMount(){
