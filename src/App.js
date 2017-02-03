35cf3a607f2898f81a75ed1749767fb616ce1a6d
alura <alura@Alura-Azul.local>
2017-02-03 19:10:21 -0200

    react3-aula4.1-react-redux-provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Timeline from './componentes/Timeline';
-import {createStore,applyMiddleware,combineReducers} from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import {timeline} from './reducers/timeline';
-import {notificacao} from './reducers/header';
-
-const reducers = combineReducers({timeline,notificacao});
-const store = createStore(reducers,applyMiddleware(thunkMiddleware));
 
 
 class App extends Component {
@@ -15,12 +8,16 @@ class App extends Component {
     return (
     <div id="root">
       <div className="main">
-        <Header store={store}/>
-        <Timeline login={this.props.params.login} store={store}/>
+        <Header store={this.context.store}/>
+        <Timeline login={this.props.params.login} store={this.context.store}/>
       </div>
     </div>
     );
   }
+}
+
+App.contextTypes = {
+  store : React.PropTypes.object.isRequired
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import Login from './componentes/Login';
 import Logout from './componentes/Logout';
 import {Router,Route,browserHistory} from 'react-router';
 import {matchPattern} from 'react-router/lib/PatternUtils';
+import {createStore,applyMiddleware,combineReducers} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {timeline} from './reducers/timeline';
+import {notificacao} from './reducers/header';
+import {Provider} from 'react-redux';
 
 function verificaAutenticacao(nextState,replace) {
   const resultado = matchPattern('/timeline(/:login)',nextState.location.pathname);
@@ -18,14 +23,20 @@ function verificaAutenticacao(nextState,replace) {
   }
 }
 
+const reducers = combineReducers({timeline,notificacao});
+const store = createStore(reducers,applyMiddleware(thunkMiddleware));
+
+
 
 ReactDOM.render(
   (
-    <Router history={browserHistory}>
-      <Route path="/" component={Login}/>
-      <Route path="/timeline(/:login)" component={App} onEnter={verificaAutenticacao}/>      
-      <Route path="/logout" component={Logout}/>
-    </Router>
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path="/" component={Login}/>
+        <Route path="/timeline(/:login)" component={App} onEnter={verificaAutenticacao}/>      
+        <Route path="/logout" component={Logout}/>
+      </Router>
+    </Provider>
   ),
   document.getElementById('root')
 );
